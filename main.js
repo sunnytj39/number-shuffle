6b7a882ab79407b45399cfbc6d52c005e03a6f6e
Kenta Kato <sunnytj39@gmail.com>
2018-04-19 10:46:52 +0900

    Add after

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 /* usage
- * shuffleNumber(対象の要素, シャッフルの間隔, 1文字が変わる回数);
+ * shuffleNumber(対象の要素, シャッフルの間隔, 1文字が変わる回数, 結果);
  */
 var element = document.getElementById('target');
-shuffleNumber(element, 100, 10);
+shuffleNumber(element, 100, 10, 256);
 
 /******************************************************************/
 
-function shuffleNumber(element, speed = 50, time = 500) {
+function shuffleNumber(element, speed = 50, time = 500, after = 25) {
   // 対象の文字長を取得
   const numLength = element.textContent.length;
+  // 結果後の桁数
+  const resultLength = after.toString(10).length;
   // 結果の文字列
   let result = '';
 
@@ -25,13 +27,18 @@ function shuffleNumber(element, speed = 50, time = 500) {
         }
         // timeに達するまで繰り返す
         if(count < time) {
-        　// テキストを変更
           element.textContent = result + shuffleStr;
           count++;
         } else {
-          element.textContent = result + shuffleStr;
-          // 生成したランダムな文字列の先頭を結果に追加
-          result += shuffleStr.slice(0, 1);
+          if (remain == resultLength - 1) {
+            // 結果を代入
+            result += after.toString(10).slice(remain, remain + 1);
+            element.textContent = result;
+          } else {
+            result += after.toString(10).slice(remain, remain + 1);
+            element.textContent = result + shuffleStr;
+          }
+          // Intervalを解除
           clearInterval(si);
           // resolveを返す
           resolve();
@@ -42,7 +49,7 @@ function shuffleNumber(element, speed = 50, time = 500) {
 
   // async/awaitで処理を待ってループ
   async function start() {
-    for (var i = 0; i < numLength; i++) {
+    for (var i = 0; i < resultLength; i++) {
       const res = await shuffle(i);
     }
   }
